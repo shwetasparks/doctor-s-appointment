253819164792b2d7e22df3c66239c6ad37f43280
Shweta <shwetakoffficial@gmail.com>
2024-10-05 21:51:01 +0530

    Top Doctor UI

---
+++ frontend/src/components/context/AppContext.jsx
@@ -0,0 +1,10 @@
+import { createContext } from "react";
+
+export const AppContext=createContext()
+
+//context provider function
+const AppContextProvider=()=>{
+    const value={
+        
+    }
+}
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,12 +1,14 @@
 
 import FindBySpeciality from "../components/FindBySpeciality"
 import Header from "../components/Header"
+import TopDoctors from "../components/TopDoctors"
 
 function Home() {
     return (
         <div>
             <Header/>
             <FindBySpeciality/>
+            <TopDoctors/>
             
             
           
